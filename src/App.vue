f2cd79cbbf4d25d914a04d48aa9849abb4074aae
Paul Barthmaier <pbrocks@users.noreply.github.com>
2023-04-11 10:02:30 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -43,8 +43,8 @@ export default {
 </script>
 
 <style>
-body{
-    margin: 0;
-    font-family: 'Nunito SemiBold';
-}
-</style>
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap');
+  body{
+      margin: 0;
+      font-family: 'Nunito';
+  }</style>
